f098171a25e8fd855b1980bbce79e9e4a42be13a
Saket Thakare <saketthakare@gmail.com>
2018-11-18 13:07:59 -0800

    Add access control for job routes

--- backend/src/routes/api/jobs.route.js
+++ backend/src/routes/api/jobs.route.js
@@ -6,11 +6,11 @@ const auth = require('../../middlewares/authorization')
 const jobsController = require('../../controllers/jobs.controller')
 
 router.get('/', auth(), jobsController.get)
-router.post('/', auth(), jobsController.post)
+router.post('/', auth(['recruiter']), jobsController.post)
 router.get('/:jobId', auth(), jobsController.getOne)
-router.put('/:jobId', auth(), jobsController.putOne)
-router.delete('/:jobId', auth(), jobsController.deleteOne)
-// router.post('/:jobId/save', auth(), jobsController.postOneSave)
-// router.post('/:jobId/apply', auth(), jobsController.postOneApply)
+router.put('/:jobId', auth(['recruiter']), jobsController.putOne)
+router.delete('/:jobId', auth(['recruiter']), jobsController.deleteOne)
+// router.post('/:jobId/save', auth(['applicant']), jobsController.postOneSave)
+// router.post('/:jobId/apply', auth(['applicant']), auth(), jobsController.postOneApply)
 
 module.exports = router
