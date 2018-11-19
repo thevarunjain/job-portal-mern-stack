b074d0396fb7f3a29853eba566fddde912cdd16e
Saket Thakare <saketthakare@gmail.com>
2018-11-18 16:53:47 -0800

    Update POST for /:jobId/apply with pointer references

--- backend/src/controllers/application.controller.js
+++ backend/src/controllers/application.controller.js
@@ -1,19 +1,34 @@
 'use strict'
 
 const httpStatus = require('http-status')
-// const mongoose = require('mongoose')
+const sql = require('./../services/sql')
+const mongoose = require('mongoose')
 const APIError = require('../utils/APIError')
-// const Job = require('../models/job.model')
+const Job = require('../models/job.model')
 // const Applicant = require('../models/applicant.model')
 const Application = require('../models/application.model')
 
 exports.apply = async (req, res, next) => {
   try {
     // console.log('\n\n\n', req.user.role, '\n\n\n')
+    if (!mongoose.Types.ObjectId.isValid(req.params.jobId)) throw new APIError(`Invalid jobId`, httpStatus.BAD_REQUEST)
     const response = { payLoad: {} }
-    const applicaiton = new Application(req.body)
-    const savedApplication = await applicaiton.save()
+    const applicationData = req.body
+    applicationData.jobId = req.params.jobId
+    const jobData = await Job.findById(req.params.jobId).exec()
+    if (!jobData) throw new APIError(`Invalid jobId`, httpStatus.INTERNAL_SERVER_ERROR)
+    applicationData.recruiterId = jobData.recruiter
+    applicationData.applicantId = req.user._id
+    const application = new Application(applicationData)
+    const savedApplication = await application.save()
     if (!savedApplication) throw new APIError(`Job not created`, httpStatus.INTERNAL_SERVER_ERROR)
+    const applicationPointers = {
+      'job_id': savedApplication.jobId,
+      'applicant_id': savedApplication.applicantId,
+      'recruiter_id': savedApplication.recruiterId,
+      'application_id': savedApplication._id
+    }
+    await sql.query('INSERT INTO job_application SET ?', applicationPointers)
     response.payLoad = savedApplication
     res.status(httpStatus.OK)
     res.send(response)
