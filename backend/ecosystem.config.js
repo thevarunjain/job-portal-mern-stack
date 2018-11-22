dc0cd87939be1a99dae3a6ac1bc78a3b47be0538
Mayur Barge <bargemayur05@gmail.com>
2018-11-22 13:31:47 -0800

    Production deployment from master branch deploy script to copy env file to production

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -12,11 +12,12 @@ module.exports = {
     production: {
       key: "~/.ssh/id_rsa",
       user: "ec2-user",
-      host: ["54.183.95.207"],
+      host: ["54.183.95.207", "18.144.19.140", "18.144.70.163"],
       ssh_options: "StrictHostKeyChecking=no",
-      ref: "origin/backend_deployment",
+      ref: "origin/master",
       repo: "git@github.com:saketthakare/cmpe-273-group-project.git",
       path: "/home/ec2-user/linkedin",
+      "pre-deploy-local": "./deploy_env",
       "post-deploy": "cd backend && npm install && pm2 startOrRestart ecosystem.config.js"
     }
   }
