a90d69981990acd987d345c6450a117b9cb21dc1
Saket Thakare <saketthakare@gmail.com>
2018-11-24 10:07:14 -0800

    Fix null response issue

--- backend/src/controllers/document.controller.js
+++ backend/src/controllers/document.controller.js
@@ -5,12 +5,11 @@ const APIError = require('../utils/APIError')
 
 exports.upload = async (req, res, next) => {
   try {
-    console.log(req.files)
     if (!req.files) { throw new APIError('File is not recived') }
     const response = { payLoad: [] }
     for (let index = 0; index < req.files.length; index++) {
       const element = req.files[index]
-      response.payLoad.push(element.filename)
+      response.payLoad.push(element.key)
     }
     res.status(httpStatus.CREATED)
     res.send(response)
